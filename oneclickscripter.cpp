475d0efb50cf88da6e36a638284220b87bb85c1b
MasterJayanX <57007976+MasterJayanX@users.noreply.github.com>
2023-12-18 15:28:17 -0300

    Update 1.1.0

---
+++ oneclickscripter.cpp
@@ -0,0 +1,294 @@
+// -*- coding: utf-8 -*-
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <locale.h>
+#include <map>
+#include "oneclickscripter.hpp"
+/*Programa hecho por MasterJayanX*/
+#define INICIO 1000
+
+using namespace std;
+
+bool install_winget = false;
+bool install_brew = false;
+string language;
+
+void programas(string os, ofstream& script, Translator translator){
+    char opcion = 'y';
+    bool first = true;
+    string pack, instruccion;
+    while(opcion == 'y' || opcion == 'Y'){
+        if(first){
+            cout << translator.translate("addapps") << endl;
+            cin >> opcion;
+        }
+        if(opcion == 'n' || opcion == 'N'){
+            break;
+        }
+        cout << "" << endl;
+        cout << translator.translate("addpackage") << endl;
+        if(first){
+            if(os == "Windows"){
+                cout << translator.translate("forwindows") << endl;
+            }
+            else if(os == "macOS"){
+                cout << translator.translate("formacos") << endl;
+            }
+            else if(os == "Ubuntu/Debian"){
+                cout << translator.translate("forubuntu") << endl;
+                cout << translator.translate("fordebian") << endl;
+                cout << translator.translate("forotherapt") << endl;
+            }
+            else if(os == "Arch"){
+                cout << translator.translate("forarch") << endl;
+            }
+            else if(os == "Fedora"){
+                cout << translator.translate("forfedora") << endl;
+            }
+            else if(os == "OpenSUSE"){
+                cout << translator.translate("foropensuse") << endl;
+            }
+            else if(os == "Flatpak"){
+                cout << translator.translate("forflatpak") << endl;
+            }
+            else{
+                cout << translator.translate("nonvalid") << endl;
+                opcion = 1;
+            }
+            first = false;
+        }
+        cout << translator.translate("packagename");
+        cin >> pack;
+        if(os == "Windows"){
+            instruccion = "winget install " + pack;
+        }
+        else if(os == "macOS"){
+            instruccion = "brew install " + pack;
+        }
+        else if(os == "Ubuntu/Debian"){
+            instruccion = "sudo apt install " + pack + " -y";
+        }
+        else if(os == "Arch"){
+            instruccion = "sudo pacman -S " + pack + " --noconfirm";
+        }
+        else if(os == "Fedora"){
+            instruccion = "sudo dnf install " + pack + " -y";
+        }
+        else if(os == "OpenSUSE"){
+            instruccion = "sudo zypper install " + pack + " -y";
+        }
+        else if(os == "Flatpak"){
+            instruccion = "flatpak install " + pack + " -y";
+        }
+        else{
+            cout << translator.translate("nonvalid") << endl;
+            opcion = 1;
+        }
+        script << instruccion << endl;
+        cout << translator.translate("addmoreapps") << endl;
+        cin >> opcion;
+    }
+}
+
+void script(string os, string update, Translator translator){
+    char personalizado;
+    string nombre, ext;
+    if(os == "Windows"){
+        ext = ".bat";
+    }
+    else{
+        ext = ".sh";
+    }
+    cout << translator.translate("qcustomname") << endl;
+    cin >> personalizado;
+    if(personalizado == 'y' || personalizado == 'Y'){
+        cout << translator.translate("customname") << endl;
+        cin >> nombre;
+        nombre += ext;
+    }
+    else if(personalizado == 'n' || personalizado == 'N'){
+        nombre = os + ext;
+    }
+    else{
+        cout << translator.translate("nocustomname") << endl;
+        nombre = os + ext;
+    }
+    ofstream script;
+    script.open(nombre);
+    char msj;
+    cout << translator.translate("qwelcomemsg") << endl;
+    cin >> msj;
+    if(msj == 'y' || msj == 'Y'){
+        string mensaje;
+        cout << translator.translate("welcomemsg") << endl;
+        cin.ignore();
+        getline(cin, mensaje);
+        if(os == "Windows"){
+            script << "@echo off" << endl;
+            script << "echo " << mensaje << endl;
+        }
+        else{
+            script << "#!/bin/bash" << endl;
+            script << "echo " << mensaje << endl;
+        }
+    }
+    else if(msj == 'n' || msj == 'N'){
+        cout << translator.translate("nowelcomemsg") << endl;
+        if(os == "Windows"){
+            script << "@echo off" << endl;
+        }
+        else{
+            script << "#!/bin/bash" << endl;
+        }
+    }
+    else{
+        cout << translator.translate("nonvalid") << endl;
+        if(os == "Windows"){
+            script << "@echo off" << endl;
+        }
+        else{
+            script << "#!/bin/bash" << endl;
+        }
+    }
+    if(os == "Windows"){
+        if(install_winget){
+            script << "echo " + translator.translate("instwinget") << endl;
+            script << "powershell -command \"Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\"" << endl;
+        }
+    }
+    if(os == "macOS"){
+        if(install_brew){
+            script << "echo " + translator.translate("instbrew") << endl;
+            script << "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"" << endl;
+        }
+    }
+    script << "echo " + translator.translate("updates") << endl;
+    script << update << endl;
+    programas(os, script, translator);
+    script.close();
+}
+
+int main(){
+    setlocale(LC_ALL, "");
+    int opcion = INICIO;
+    string os, update;
+    int lang;
+    cout << "Select your language / Selecciona tu idioma: " << endl << "1. English" << endl << "2. Español" << endl;
+    cin >> lang;
+    if(lang == 1){
+        language = "en";
+    }
+    else if(lang == 2){
+        language = "es";
+    }
+    else{
+        cout << "Invalid option." << endl;
+        return 0;
+    }
+    Translator translator(language);
+    cout << translator.translate("welcome") << endl;
+    while(opcion == INICIO){
+        cout << translator.translate("option") << endl << translator.translate("opt1") << endl << translator.translate("opt2") << endl << translator.translate("opt3") << endl << translator.translate("opt4") << endl << translator.translate("opt5") << endl;
+        cin >> opcion;
+        if(opcion == 5){
+            return 0;
+        }
+        else if(opcion == 1){
+            os = "Windows";
+            update = "winget upgrade -h --all";
+            char winget = 'a';
+            while(winget != 'y' && winget != 'Y' && winget != 'n' && winget != 'N'){
+                cout << translator.translate("winget") << endl;
+                cin >> winget;
+                if(winget == 'y' || winget == 'Y'){
+                    install_winget = true;
+                }
+                else if(winget == 'n' || winget == 'N'){
+                    cout << translator.translate("nowinget") << endl;
+                    install_winget = false;
+                }
+                else{
+                    cout << translator.translate("nonvalid") << endl;
+                    install_winget = false;
+                }
+            }
+            script(os, update, translator);
+        }
+        else if(opcion == 2){
+            os = "macOS";
+            update = "softwareupdate -i -a";
+            char brew = 'a';
+            while(brew != 'y' && brew != 'Y' && brew != 'n' && brew != 'N'){
+                cout << translator.translate("homebrew") << endl;
+                cin >> brew;
+                if(brew == 'y' || brew == 'Y'){
+                    install_brew = true;
+                }
+                else if(brew == 'n' || brew == 'N'){
+                    install_brew = false;
+                    cout << translator.translate("nohomebrew") << endl;
+                }
+                else{
+                    cout << translator.translate("nonvalid") << endl;
+                    install_brew = false;
+                }
+            }
+            script(os, update, translator);
+        }
+        else if(opcion == 3){
+            int opcion2;
+            cout << translator.translate("distro") << endl << "1. Ubuntu/Debian" << endl << "2. Arch" << endl << "3. Fedora" << endl << "4. OpenSUSE" << endl << "5. " << translator.translate("flat") << endl << "6. " << translator.translate("distroback") << endl;
+            cin >> opcion2;
+            if(opcion2 == 1){
+                os = "Ubuntu/Debian";
+                update = "sudo apt update && sudo apt upgrade -y";
+            }
+            else if(opcion2 == 2){
+                os = "Arch";
+                update = "sudo pacman -Syu";
+            }
+            else if(opcion2 == 3){
+                os = "Fedora";
+                update = "sudo dnf upgrade -y";
+            }
+            else if(opcion2 == 4){
+                os = "OpenSUSE";
+                update = "sudo zypper update -y";
+            }
+            else if(opcion2 == 5){
+                os = "Flatpak";
+                update = "flatpak update -y";
+            }
+            else if(opcion2 == 6){
+                opcion = INICIO;
+            }
+            else{
+                cout << translator.translate("nonvalid") << endl;
+                opcion = INICIO;
+            }
+            if(opcion2 != 6 && opcion != INICIO){
+                script(os, update, translator);
+            }
+        }
+        else if(opcion == 4){
+            cout << translator.translate("about1") << endl;
+            cout << translator.translate("about2") << endl;
+            cout << translator.translate("about3") << endl;
+            cout << translator.translate("about4") << endl;
+            cout << translator.translate("presskey") << endl;
+            cin.ignore();
+            cin.get();
+            opcion = INICIO;
+        }
+        else{
+            cout << translator.translate("nonvalid") << endl;
+            opcion = INICIO;
+        }
+    }
+    cout << translator.translate("presskey") << endl;
+    cin.ignore();
+    cin.get();
+    return 0;
+}
---
+++ oneclickscripter.hpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class Translator {
+public:
+    Translator(const string& language) {
+        loadTranslations(language);
+    }
+
+    string translate(const string& key) const {
+        auto it = translations.find(key);
+        return (it != translations.end()) ? it->second : "Translation not found";
+    }
+
+private:
+    map<string, string> translations;
+
+    void loadTranslations(const string& language) {
+        if (language == "en") {
+            translations["welcome"] = "Welcome to OneClickAppInstaller, a software that allows you to create a script that installs Windows or Linux applications with a single click. This software was created by MasterJayanX.";
+            translations["option"] = "Select an option: ";
+            translations["opt1"] = "1. Create script for Windows";
+            translations["opt2"] = "2. Create script for macOS";
+            translations["opt3"] = "3. Create script for Linux";
+            translations["opt4"] = "4. About";
+            translations["opt5"] = "5. Exit";
+            translations["nonvalid"] = "Invalid option.";
+            translations["winget"] = "This script uses the winget package manager to install applications. Do you want the script to install winget? [y/n]";
+            translations["nowinget"] = "Winget will not be installed.";
+            translations["homebrew"] = "This script uses the Homebrew package manager to install applications. Do you want the script to install Homebrew (brew)? [y/n]";
+            translations["nohomebrew"] = "Homebrew will not be installed.";
+            translations["distro"] = "Select your Linux distribution:";
+            translations["flat"] = "Flatpak (for other distributions)";
+            translations["distroback"] = "Return to Main Menu";
+            translations["qcustomname"] = "Do you want to give your script a custom name? [y/n]";
+            translations["customname"] = "Enter a name for your file:";
+            translations["nocustomname"] = "Invalid option. A custom name will not be applied.";
+            translations["qwelcomemsg"] = "Do you want to add a custom welcome message to the script? [y/n]";
+            translations["welcomemsg"] = "Enter your message:";
+            translations["nowelcomemsg"] = "No welcome message will be added.";
+            translations["instwinget"] = "Installing winget...";
+            translations["instbrew"] = "Installing brew...";
+            translations["updates"] = "Installing updates...";
+            translations["addapps"] = "Do you want to add applications to the script? [y/n]";
+            translations["addmoreapps"] = "Do you want to add more applications to the script? [y/n]";
+            translations["addpackage"] = "Enter the package name of the application you want to install. Note that the package name must be a valid package name for the script to work correctly.";
+            translations["forwindows"] = "For Windows (winget) users, you can find valid package names here: https://winget.run (you don't need to copy the part that says winget install, just the package ID).";
+            translations["formacos"] = "For macOS (brew) users, you can find valid package names here: https://formulae.brew.sh (you don't need to copy the part that says brew install, just the package name).";
+            translations["forubuntu"] = "For Ubuntu (apt) users, you can find valid package names here: https://packages.ubuntu.com/ (you don't need to copy the part that says sudo apt install, just the package name).";
+            translations["fordebian"] = "For Debian (apt) users, you can find valid package names here: https://packages.debian.org/en/ (also you don't need to copy the part that says sudo apt install, just the package name).";
+            translations["forotherapt"] = "For other Ubuntu or Debian based distributions, you can find valid package names on their respective websites.";
+            translations["forarch"] = "For Arch (pacman) users, you can find valid package names here: https://archlinux.org/packages/ (you don't need to copy the part that says sudo pacman -S, just the package name).";
+            translations["forfedora"] = "For Fedora (dnf) users, you can find valid package names here: https://apps.fedoraproject.org/packages/ (no need to copy the part that says sudo dnf install, just the package name) .";
+            translations["foropensuse"] = "For OpenSUSE (zypper) users, you can find valid package names here: https://software.opensuse.org/ (you don't need to copy the part that says sudo zypper install, just the package name).";
+            translations["forflatpak"] = "For Flatpak users, you can find valid package names here: https://flathub.org/home (you don't need to copy the part that says flatpak install, just the package name).";
+            translations["packagename"] = "Enter the package name here:";
+            translations["about1"] = "OneClickAppInstaller is a simple command line tool that allows you to create a script to install all your applications on Windows, macOS or Linux automatically with just one click.";
+            translations["about2"] = "This tool was created by MasterJayanX.";
+            translations["about3"] = "Version: 1.1.0 (2023-12-18)";
+            translations["about4"] = "Check out the full changelog here: https://github.com/MasterJayanX/OneClickAppInstaller/releases";
+            translations["presskey"] = "Press Enter to exit...";
+        } 
+        else if (language == "es") {
+            translations["welcome"] = "Bienvenido a OneClickAppInstaller, un software que te permite crear un script que instala aplicaciones de Windows o Linux con un solo clic. Este software fue creado por MasterJayanX.";
+            translations["option"] = "Selecciona una opción: ";
+            translations["opt1"] = "1. Crear script para Windows";
+            translations["opt2"] = "2. Crear script para macOS";
+            translations["opt3"] = "3. Crear script para Linux";
+            translations["opt4"] = "4. Acerca de";
+            translations["opt5"] = "5. Salir";
+            translations["nonvalid"] = "Opción no válida.";
+            translations["winget"] = "Este script hace uso del gestor de paquetes winget para instalar las aplicaciones. ¿Deseas que el script instale winget? [y/n]";
+            translations["nowinget"] = "No se instalará winget.";
+            translations["homebrew"] = "Este script hace uso del gestor de paquetes Homebrew para instalar las aplicaciones. ¿Deseas que el script instale Homebrew (brew)? [y/n]";
+            translations["nohomebrew"] = "No se instalará Homebrew.";
+            translations["distro"] = "Selecciona tu distribución Linux:";
+            translations["flat"] = "Flatpak (para otras distribuciones)";
+            translations["distroback"] = "Volver al inicio";
+            translations["qcustomname"] = "¿Deseas ponerle un nombre personalizado a tu script? [y/n]";
+            translations["customname"] = "Escribe un nombre para tu archivo:";
+            translations["nocustomname"] = "Opción no válida. No se aplicará un nombre personalizado.";
+            translations["qwelcomemsg"] = "¿Deseas agregar un mensaje de bienvenida personalizado al script? [y/n]";
+            translations["welcomemsg"] = "Escribe tu mensaje:";
+            translations["nowelcomemsg"] = "No se agregará un mensaje de bienvenida.";
+            translations["instwinget"] = "Instalando winget...";
+            translations["instbrew"] = "Instalando brew...";
+            translations["updates"] = "Instalando actualizaciones...";
+            translations["addapps"] = "¿Deseas agregar aplicaciones al script? [y/n]";
+            translations["addmoreapps"] = "¿Deseas agregar más aplicaciones al script? [y/n]";
+            translations["addpackage"] = "Ingrese el nombre del paquete de la aplicación que deseas instalar. Ten en cuenta que el nombre del paquete debe ser un nombre de paquete válido para que el script funcione correctamente.";
+            translations["forwindows"] = "Para usuarios de Windows (winget), pueden encontrar los nombres de paquetes válidos aquí: https://winget.run (no es necesario copiar la parte que dice winget install, solo el ID del paquete).";
+            translations["formacos"] = "Para usuarios de macOS (brew), pueden encontrar los nombres de paquetes válidos aquí: https://formulae.brew.sh (no es necesario copiar la parte que dice brew install, solo el nombre del paquete).";
+            translations["forubuntu"] = "Para usuarios de Ubuntu (apt), pueden encontrar los nombres de paquetes válidos aquí: https://packages.ubuntu.com/ (no es necesario copiar la parte que dice sudo apt install, solo el nombre del paquete).";
+            translations["fordebian"] = "Para usuarios de Debian (apt), pueden encontrar los nombres de paquetes válidos aquí: https://packages.debian.org/en/ (igualmente no es necesario copiar la parte que dice sudo apt install, solo el nombre del paquete).";
+            translations["forotherapt"] = "Para otras distribuciones basadas en Ubuntu o Debian, pueden encontrar los nombres de paquetes válidos en sus respectivos sitios web.";
+            translations["forarch"] = "Para usuarios de Arch (pacman), pueden encontrar los nombres de paquetes válidos aquí: https://archlinux.org/packages/ (no es necesario copiar la parte que dice sudo pacman -S, solo el nombre del paquete).";
+            translations["forfedora"] = "Para usuarios de Fedora (dnf), pueden encontrar los nombres de paquetes válidos aquí: https://apps.fedoraproject.org/packages/ (no es necesario copiar la parte que dice sudo dnf install, solo el nombre del paquete).";
+            translations["foropensuse"] = "Para usuarios de OpenSUSE (zypper), pueden encontrar los nombres de paquetes válidos aquí: https://software.opensuse.org/ (no es necesario copiar la parte que dice sudo zypper install, solo el nombre del paquete).";
+            translations["forflatpak"] = "Para usuarios de Flatpak, pueden encontrar los nombres de paquetes válidos aquí: https://flathub.org/home (no es necesario copiar la parte que dice flatpak install, solo el nombre del paquete).";
+            translations["packagename"] = "Escribe el nombre del paquete aquí:";
+            translations["about1"] = "OneClickAppInstaller es una herramienta de línea de comandos sencilla que te permite crear un script para instalar todas tus aplicaciones en Windows, macOS o Linux automáticamente con solo un clic.";
+            translations["about2"] = "Esta herramienta fue creada por MasterJayanX.";
+            translations["about3"] = "Versión: 1.1.0 (2023-12-18)";
+            translations["about4"] = "Revisa el historial de cambios completo aquí: https://github.com/MasterJayanX/OneClickAppInstaller/releases";
+            translations["presskey"] = "Presiona Enter para salir...";
+        }
+    }
+};
